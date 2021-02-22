fc6e018bf6adf57d753de45f0e64322338ccc3c2
a7474267 <a7474267@gmail.com>
2021-02-22 21:30:19 +0800

    add vue

--- W8-Vue/vue.config.js
+++ W8-Vue/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/W8-Vue/'
+        ? '/2020-Fall-WebDev/'
         : './',
     outputDir: "dist",
     lintOnSave: true,
