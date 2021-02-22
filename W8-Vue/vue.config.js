b73c5a5d775de4c9ffb30e1bfe7abf12b643692b
a7474267 <a7474267@gmail.com>
2021-02-22 16:44:30 +0800

    fix public path

--- W8-Vue/vue.config.js
+++ W8-Vue/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/W8-Vue/dist/'
+        ? '/W8-Vue/'
         : './',
     outputDir: "dist",
     lintOnSave: true,
